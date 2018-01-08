8fed351c9c82c178658e8470137290a62c4e0dc1
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-08 10:29:43 +0000

    Create set1.2.c

---
+++ set1.2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int number;
+printf("enter the integer");
+scanf("%d",&number);
+if(number%2==0)
+{
+printf("%d" is even",number);
+}
+else
+{
+printf("%d is odd",number);
+}
+return 0;
+}
