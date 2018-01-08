ae30ea1ce2a5fd123bee4a4ffa9d62e831446dbb
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-08 10:25:09 +0000

    Create set1.1.c

---
+++ set1.1.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the n value");
+scanf("%d",&n);
+if(n>0)
+{
+printf("positive number");
+}
+elseif
+{
+printf(negative number");
+}
+else
+{
+printf("zero");
+}
+}
+
