19617ae767832ec60c1ae908f700997b4fa1111f
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-08 10:39:34 +0000

    Create setaaa2.c

---
+++ setaaa2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter the integer");
+scanf("%d",&num);
+if(number%2==0)
+{
+printf("%d" is even",num);
+}
+else
+{
+printf("%d is odd",num);
+}
+return 0;
+}
+
