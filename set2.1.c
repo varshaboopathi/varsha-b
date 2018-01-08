1743851681964d188f45d78cc5643540a669c357
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-08 11:16:00 +0000

    Create set2.1.c

---
+++ set2.1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+[
+int base,exponent;
+long long result=1;
+scanf("%d",&base0;
+scanf("%d",&exponent);
+while(exponent!=0)
+{
+result*=base;
+--exponent;
+printf("answer="%lld",result);
+}}
