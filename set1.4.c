d8db54dd214e5d98923dfb9249750ba64eb6dc28
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-08 10:37:23 +0000

    Create set1.4.c

---
+++ set1.4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char s;
+printf("enter the character");
+scanf("%c',&c);
+if((s>='a'&&c<='z')||(c>='A'&&c<='z'0)
+printf("%c is an alphabet",s);
+else
+printf("%c is not an alphabet",s);
+return 0;
+}
