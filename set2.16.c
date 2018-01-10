9b4711e286c1389cd89d5c8ac8544c12dfc6aeee
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-10 10:34:44 +0000

    Create set2.16.c

---
+++ set2.16.c
@@ -0,0 +1,23 @@
+#incclude,stdio.h>
+void main()
+{
+int low,high,i,flag;
+scanf("%d%d",&low,&high);
+printf("{prime numbers between%d and %d are',low,high);
+while(low<high>
+{
+flag=0;
+for(i=2;i<=low/2;++i)
+{
+if(low%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==)
+printf("%d",low);
+++low;
+}
+return o;
+}
