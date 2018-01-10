93acf22320beb86def640d6c3990b4e6b4d0cb3d
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-10 10:42:03 +0000

    Create set2.19.c

---
+++ set2.19.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+unsigned long long factorial=1;
+scanf("%d",&n);
+if(n<0)
+printf("error!factorial of a negative number does not exist");
+else
+{
+for(i=1;i<=n;++i)
+{
+factoria*=i;
+}
+printf("factorial of %d=%llu"",n,factorial);
+}
+return o;
+}
