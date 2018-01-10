fd06fee35e84abc5266d7e03c2b6fe5338d1d69e
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-10 10:27:41 +0000

    Create set2.12.c

---
+++ set2.12.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int n,a=0,b,e;
+scanf("%d",&n);
+c=n;
+while(n!=0)
+{
+remainder=n%10;
+a=a*10+b;
+n/=10;
+}
+if(e==a)
+{
+printf("%d is palindrome",e);
+]
+else
+{
+printf("%d is not a palindrome",e);
+}
+return 0;
+]
