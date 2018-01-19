2c9be6b282a07b311073b23606258c74454394d0
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-19 10:17:22 +0000

    Create expo.c

---
+++ expo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+[
+int base,expo;
+long long result=1;
+scanf("%d",&base0;
+scanf("%d",&expon);
+while(expo!=0)
+{
+result*=base;
+--expo;
+printf("answer="%lld",result);
+}}
