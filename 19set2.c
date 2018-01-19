090310f42db83fb42f1df9db63cacddada63c263
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-19 10:32:40 +0000

    Create 19set2.c

---
+++ 19set2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a ,i;
+scanf("%d",&a);
+if(a<0)
+printf("error!factorial of a naegative number does not exist");
+else
+{
+for(i=1;i<=a;++i)
+{
+factoria*=i;
+}
+printf("factorial of %d=%llu"",a,factorial);
+}
+return o;
+}
+
