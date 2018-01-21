2053ee7250661e22ce01950b4b1e3cdc35fa7a23
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-21 14:09:56 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int p,i,sum=0,add=0;
+scanf("%d",&p);
+for(i=1;i<=15;i++)
+{
+add+=i;
+}
+printf("the sum of numbers betwwn 1 and 15 is %d",add);
+while(p<=45)
+{
+if(p%3==0)
+{
+sum+=a;
+}
+p++;
+}
+printf("odd numbers between 15 and 45 is %d",sum);
+return 0;
+}
