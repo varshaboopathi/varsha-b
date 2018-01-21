7e009db475eaf10e88e25125efac18a555f63f3c
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-21 14:52:39 +0530

    Create 40s.c

---
+++ 40s.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a=0,b=1,c,n,count=0;
+scanf("%d",&n);
+printf("first %d FIBONACCI numbers are   \n",n);
+printf("%d",a);
+printf("%d",b);
+count=2;
+while(count<n)
+{
+c=a+b;
+count++;
+printf("%d",c);
+a=b;
+b=c;
+}
+}
