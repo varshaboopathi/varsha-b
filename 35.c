e854b18bb82b3ac9cb1ecc0ef54249aee44ee76a
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-21 14:34:53 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+   char str[100];
+   int i = 0, l = 0, f = 1;
+   printf("Enter any string\n");
+   scanf("%s",&str);
+   if(str==1 || str==9)
+   {
+      l=l++;
+   printf("the no of charcter in string is %d\n",l)
+   getch();
+   }
