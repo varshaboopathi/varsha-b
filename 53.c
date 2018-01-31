e624d07579e5c79055c7a5431ca30806264efdd3
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-31 16:11:23 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[5],b,i;
+printf("enter elements");
+for(i=0;i<=5;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=5;i++)
+{
+b=a[i]+b;
+}
+printf(" the sum is %d",b);
+}
