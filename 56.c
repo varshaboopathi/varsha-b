a50916cdac27c6eb435177c90631a80c4e69c7dc
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-01-31 16:56:05 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int s;
+char a,i;
+scanf("%s%d",&a,&s);
+{
+if(((i=='a')&&(i=='z'))||((i=='A' )&&(i=='Z')))
+{
+printf("contains only alphabet");
+}
+else
+{
+printf("contains number and alphabet");
+}
+}
+}
