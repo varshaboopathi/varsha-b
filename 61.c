ee765f72d27981f0852034c3ee438c8fe9c4b9bc
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-02-02 14:57:15 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char s[50];
+int a,i;
+printf("enter the character");
+scanf("%s",&s);
+printf("enter the number");
+scanf("%d",&a);
+{
+for(i=0;i<a;i++)
+{
+printf("%s",s[i]);
+}
