776354957cc2bb3764f9705df07a477d6332e8d0
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-02-05 16:02:01 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char a[20];
+int i,b=1,c;
+printf("enter the string");
+scanf("%s",&a[i]);
+for(i=0;a[i]!='\0';i++)
+{
+    if(a[i]==' ')
+    {
+   ++b;
+    }
+}
+printf("%d",++b);
+}
+
