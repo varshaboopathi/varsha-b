f7379bab85a03367faf308a6d97b988a40692431
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-02-12 16:03:23 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+char s[10];
+scanf("%c",&s);
+for(i=0;i<1;i++)
+if((s[i]=='a')||(s[i]='e')||(s[i]=='i')||(s[i]=='o')||(s[i]=='u'))
+{
+printf("has vowel ");
+}
+else
+{
+printf("has no vowel ");
+}
+}
