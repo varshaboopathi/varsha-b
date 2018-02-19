3875144022fdcb9fc61cf473edddfde83c4aaece
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-02-19 15:58:04 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+scanf("%d%d",&a,&b);
+c=a*b;
+printf("the p[roduct is %d",c);
+if(a==b)
+{
+printf("perfect square");
+}
+else
+{
+printf("not perfect square");
+}
+}
