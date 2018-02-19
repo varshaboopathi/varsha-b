1d5b679efe083c774e17d27ef0ce93dac4109fe5
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-02-19 16:28:41 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,a1,b,b1,c,c1;
+scanf("%d%d\n",&a,&a1);
+scanf("%d%d\n",&b,&b1);
+scanf("%d%d\n",&c,&c1);
+printf("%d\n",a-a1);
+printf("%d\n",b-b1);
+printf("%d\n",c-c1);
+
+}
