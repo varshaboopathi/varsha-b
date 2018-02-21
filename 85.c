51b95f855520ebdc465ed230b938d185744a9cce
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-02-21 16:35:40 +0530

    Create 85.c

---
+++ 85.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    char a[5];
+    int i;
+    scanf("%s",&a[i]);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%c",a[i]);
+        }
+        
+    }
+    for(i=0;a[i]!='\0';i++)
+     {
+         if(i%2==0)
+        {
+          
+            printf("%c",a[i]);
+        }
+    }
+    
+    
+}
