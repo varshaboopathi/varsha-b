8365c6361d3793bc5632b6be3073f24fb4693005
varshaboopathi <30829322+varshaboopathi@users.noreply.github.com>
2018-02-21 16:23:58 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int a[2],b[2],i,c,d;
+    for(i=0;i<2;i++)
+    {
+        scanf("%d",&a[i]);
+        
+    }
+    for(i=0;i<2;i++)
+    {
+        scanf("%d",&b[i]);
+       
+    }
+    printf("division is\t");
+    for(i=0;i<2;i++)
+    {
+         printf("%d",(a[i]/[i]));
+   
+    
+    }
+    printf("modulo\t");
+    for(i=0;i<2;i++)
+    {
+         printf("%d",(a[i]%\tb[i]));
+    }
+}
